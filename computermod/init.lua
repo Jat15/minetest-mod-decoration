c84c1aada9801022925c7d1d04cc2ddb493f4995
ViraXZeEscrobar <ze_escrobar@live.fr>
2014-03-31 19:31:47 +0200

    Computer_Mod
    Computer_Mod a rattacher au Decomod ... (+tard)

--- computermod/init.lua
+++ computermod/init.lua
@@ -1,5 +1,11 @@
 -- COMPUTER Mod - Code Libre Ze.-=Lua Minetest Steinheim=-
 
+
+local function has_pcsteinheim_privilege(meta, player)
+return player:get_player_name() == meta:get_string("owner") or player:get_player_name() == minetest.setting_get("name")
+end
+
+
 --REGISTER NODE
 
 minetest.register_node('computermod:pcsteinheim', {
@@ -15,11 +21,52 @@ minetest.register_node('computermod:pcsteinheim', {
 				meta:get_string("owner")..")")
 	end,
 
- can_dig = function(pos, player)
-          local meta = minetest.env:get_meta(pos)
-          local inv = meta:get_inventory()
-  return minetest.setting_get("name") == player:get_player_name() 
-     end,
+    	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("infotext", "pcsteinheim")
+		meta:set_string("owner", "")
+		local inv = meta:get_inventory()
+		inv:set_size("main", 8*4)
+	end,
+
+	can_dig = function(pos,player)
+		local meta = minetest.get_meta(pos);
+		local inv = meta:get_inventory()
+		return inv:is_empty("main") and has_pcsteinheim_privilege(meta, player)
+	end,
+	allow_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
+		local meta = minetest.get_meta(pos)
+		if not has_pcsteinheim_privilege(meta, player) then
+			minetest.log("action", player:get_player_name()..
+					" verrouille appartenant a "..
+					meta:get_string("owner").." at "..
+					minetest.pos_to_string(pos))
+			return 0
+		end
+		return count
+	end,
+    allow_metadata_inventory_put = function(pos, listname, index, stack, player)
+		local meta = minetest.get_meta(pos)
+		if not has_pcsteinheim_privilege(meta, player) then
+			minetest.log("action", player:get_player_name()..
+					" verrouille appartenant a "..
+					meta:get_string("owner").." at "..
+					minetest.pos_to_string(pos))
+			return 0
+		end
+		return stack:get_count()
+	end,
+    allow_metadata_inventory_take = function(pos, listname, index, stack, player)
+		local meta = minetest.get_meta(pos)
+		if not has_pcsteinheim_privilege(meta, player) then
+			minetest.log("action", player:get_player_name()..
+					" verrouille appartenant a "..
+					meta:get_string("owner").." at "..
+					minetest.pos_to_string(pos))
+			return 0
+		end
+		return stack:get_count()
+	end,
 
   drawtype = 'nodebox',
   tiles = {
